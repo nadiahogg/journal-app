83ec7d2ecf8e354d2f7e663981eb569f641ea65d
Nadia Hogg <hogg.nadia@gmail.com>
2018-02-23 14:23:55 -0500

    initial commit

--- dev/scripts/app.js
+++ dev/scripts/app.js
@@ -3,11 +3,23 @@ import ReactDOM from 'react-dom';
 
 class App extends React.Component {
     render() {
-      return (
-        <div>
-          Hello
-        </div>
-      )
+      return <div>
+          <header className="mainHeader">
+            <h1>Journal App</h1>
+            <nav>
+              <a href="">Add a New Story</a>
+            </nav>
+          </header>
+          <section className="entries">
+            <form action="">
+              <label htmlFor="entry-title" />
+              <input type="text" name="entry-title" placeholder="Title" />
+              <label htmlFor="entry-text" />
+              <textarea name="entry-text" placeholder="Write your story" />
+            </form>
+          </section>
+          <aside className="sidebar" />
+        </div>;
     }
 }
 
